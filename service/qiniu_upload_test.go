19483fc10c8d28981dddaedfd10d51e0cd267284
Rockyzsu <jinweizsu@gmail.com>
2022-09-26 21:45:39 +0800

    test case

--- service/qiniu_upload_test.go
+++ service/qiniu_upload_test.go
@@ -3,9 +3,22 @@ package service
 import "testing"
 
 func TestDeleteImage(t *testing.T) {
-	//t.Log("Pass")
-	url := "http://xximg.30daydo.com/picgo/1635745295943.jpg"
+	/*
+
+	 */
+	url := "http://www.com/picgo/1635745295943.jpg"
 	if !DeleteImage(url) {
 		t.Log("Failed")
 	}
+}
+
+func TestUpload(t *testing.T) {
+	file := "/home/xda/Pictures/dc321c62125c521796893b1eff0762f0.png"
+	_, err := UploadImg("/pico/testing.png", file)
+	if err != nil {
+		t.Log("Failed")
+	} else {
+		t.Log("Passed")
+	}
+
 }
